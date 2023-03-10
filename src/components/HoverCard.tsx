605371d5ffab64db58124aff492d27874d6f14d5
FlavTiBabe <flavio14hanzio@gmail.com>
2023-03-10 16:20:46 +0100

    test

--- src/components/HoverCard.tsx
+++ src/components/HoverCard.tsx
@@ -13,7 +13,7 @@ const HoverCardDemo = (profile:HoverCardProps) => (
     <HoverCard.Trigger asChild>
       <a
         className="cursor-pointer"
-        href={profile.owner.htmlUrl}
+        href={profile.url}
         target="_blank"
         rel="noreferrer noopener"
       >
@@ -48,7 +48,7 @@ const HoverCardDemo = (profile:HoverCardProps) => (
         className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-md bg-white p-5 shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
         sideOffset={5}
       >
-        <div className="flex flex-col gap-[15px]">
+        <a href={profile.owner.htmlUrl} className="flex flex-col gap-[15px]">
           <img
             className="block h-[60px] w-[60px] rounded-full object-cover"
             src={profile.owner.avatarUrl}
@@ -73,7 +73,8 @@ const HoverCardDemo = (profile:HoverCardProps) => (
               </div>
             </div>
           </div>
-        </div>
+        </a>
+
 
         <HoverCard.Arrow className="fill-white" />
       </HoverCard.Content>
